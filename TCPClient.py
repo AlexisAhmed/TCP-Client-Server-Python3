e6efc2f5edcf534acab604cb8ab0e36db56800e4
Alexis Ahmed <alexiayub34@gmail.com>
2018-07-04 22:28:48 +0300

    Update TCPClient.py

--- TCPClient.py
+++ TCPClient.py
@@ -2,6 +2,7 @@
 
 import socket
 
+#Create socket object
 clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #host = '192.168.1.104'
@@ -9,8 +10,9 @@ host = socket.gethostname()
 
 port = 444
 
-clientsocket.connect(('192.168.1.104', port))
+clientsocket.connect(('192.168.1.104', port)) #You can substitue the host with the server IP
 
+#Receiving a maximum of 1024 bytes
 message = clientsocket.recv(1024)
 
 clientsocket.close()
